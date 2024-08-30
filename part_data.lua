16c9dd6a3456b4800e5e545bbd0d75e206d74c35
Yui <naominewedits@gmail.com>
2024-08-30 16:47:21 +0200

    Changed part color

--- part_data.lua
+++ part_data.lua
@@ -2,7 +2,7 @@ return {
     Part = {
         Name = "MyPart",
         Size = Vector3.new(4, 1, 2),
-        Color = Color3.fromRGB(255, 0, 0),
+        Color = Color3.fromRGB(0, 153, 255),
         Position = workspace.PartSpawn.Position,
         Anchored = true
     }
