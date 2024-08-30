e01d01c54aed78fe401e3bc7753840235e212057
Yuma <102835658+YumaIsMe@users.noreply.github.com>
2024-08-30 16:41:25 +0200

    Update part_data.lua

--- part_data.lua
+++ part_data.lua
@@ -3,7 +3,7 @@ return {
         Name = "MyPart",
         Size = Vector3.new(4, 1, 2),
         Color = Color3.fromRGB(255, 0, 0),
-        Position = Vector3.new(0, 5, 0),
+        Position = workspace.PartSpawn.Position.
         Anchored = true
     }
 }
