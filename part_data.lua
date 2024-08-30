18345fd5aa24a4ff958829a7d6be1139f7824482
Yuma <102835658+YumaIsMe@users.noreply.github.com>
2024-08-30 16:40:01 +0200

    Update part_data.lua

--- part_data.lua
+++ part_data.lua
@@ -3,7 +3,8 @@ return {
         Name = "MyPart",
         Size = Vector3.new(4, 1, 2),
         Color = Color3.fromRGB(255, 0, 0),
-        Position = Vector3.new(0, 5, 0)
+        Position = Vector3.new(0, 5, 0),
+        Anchored = true
     }
 }
 -- Simple part.
