c8a2a117b7d379da6d87ed73f721be61b9fa8208
Yuma <102835658+YumaIsMe@users.noreply.github.com>
2024-08-30 16:31:55 +0200

    Create part_data.lua

---
+++ part_data.lua
@@ -0,0 +1,9 @@
+return {
+    Part = {
+        Name = "MyPart",
+        Size = Vector3.new(4, 1, 2),
+        Color = Color3.fromRGB(255, 0, 0),
+        Position = Vector3.new(0, 5, 0)
+    }
+}
+-- Simple part.
